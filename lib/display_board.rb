21186aba301bd4fb7507118bcb28aafb12dfbe4f
Angela Wang <ranran.wang@wework.com>
2018-02-09 03:43:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,11 +4,11 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board
-  puts "   |   | "
-  puts "---------"     
-  puts "   |   | "
-  puts "---------"  
-  puts "   |   | "  
+  puts "   |   |   "
+  puts "-----------"     
+  puts "   |   |   "
+  puts "-----------"  
+  puts "   |   |   "  
 end 
 
 display_board
